14ca1f37728e25efd0023b963188d14211f44611
meAFKOut <meafkoutback@gmail.com>
2024-12-17 12:53:03 +0000

    78864

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAINH
+#define MAINH
+#define PATH "PATH="
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+extern char **path_var;
+extern char **environ;
+char **_strtok(char *str, char delim);
+int exec_c(char **fcommand);
+char **get_command(char **);
+char **get_path(char **);
+char *find_file(char *command, int *);
+void print_prompt(int status);
+void free_path(void);
+void print_env(void);
+#endif
